17107553732936a53e834352f02ae5087af2cd8b
Dedenwrg <39203619+Dedenwrg@users.noreply.github.com>
2023-12-22 17:01:56 +0700

    Update cax.sh

--- cax.sh
+++ cax.sh
@@ -14,39 +14,52 @@ while true; do
   echo "1. Generate API Key"
   echo "2. View Balance"
   echo "3. View Order Books"
-  echo "4. orderbook information"
+  echo "4. Orderbook Information"
   echo "5. Place Order"
   echo "6. Check Open Order"
   echo "7. Cancel Order"
   echo "8. Withdraw"
-  echo "9. Check Deposits & Withdraw history"
+  echo "9. Deposit History"
   echo "10. Exit"
   read -p "Enter your choice (1/2/3/4/5/6/7/8/9/10): " CHOICE
 
   case "$CHOICE" in
     "1")
-      # Checking if API KEY is already present in the .env file
-      if [ -n "$APIKEY" ]; then
-        echo "API KEY is already registered in .env. Congratulations, you are ready for other actions!"
-        continue
+      # Asking the user if they already have an API key
+      read -p "Do you already have an API key? (1 for Yes, 2 for No): " HAS_APIKEY
+
+      if [ "$HAS_APIKEY" == "1" ]; then
+        # Asking the user to paste their API key
+        read -p "Paste your API key: " PASTED_APIKEY
+
+        # Saving the user's API key to the .env file
+        echo "APIKEY=$PASTED_APIKEY" > .env
+        echo "PAIR1=$PAIR1" >> .env
+        echo "PAIR2=$PAIR2" >> .env
+        echo "Congratulations! You have successfully added your API KEY to .env."
+      elif [ "$HAS_APIKEY" == "2" ]; then
+        # Creating a message to be signed with a timestamp as a nonce
+        MESSAGE=$(jq -nc --arg nonce "$(date +%s%N)" '$ARGS.named')
+
+        # Signing the message and saving it to a file
+        aut account sign-message "$MESSAGE" message.sig
+
+        # Making a POST request to obtain a new API key
+        API_KEY_RESPONSE=$(echo -n "$MESSAGE" | https POST "https://cax.piccadilly.autonity.org/api/apikeys" "api-sig:@message.sig")
+
+        # Obtaining the API key value from the response
+        NEW_API_KEY=$(echo "$API_KEY_RESPONSE" | jq -r .apikey)
+
+        # Saving the new API key value to the .env file
+        echo "APIKEY=$NEW_API_KEY" > .env
+        echo "API=$NEW_API_KEY" >> .env
+        echo "PAIR1=$PAIR1" >> .env
+        echo "PAIR2=$PAIR2" >> .env
+        echo "Congratulations! You have successfully obtained an API KEY and it's saved in .env."
+      else
+        echo "Invalid choice. Exiting the script."
+        exit 1
       fi
-
-      # Creating a message to be signed with a timestamp as a nonce
-      MESSAGE=$(jq -nc --arg nonce "$(date +%s%N)" '$ARGS.named')
-
-      # Signing the message and saving it to a file
-      aut account sign-message "$MESSAGE" message.sig
-
-      # Making a POST request to obtain a new API key
-      API_KEY_RESPONSE=$(echo -n "$MESSAGE" | https POST "https://cax.piccadilly.autonity.org/api/apikeys" "api-sig:@message.sig")
-
-      # Obtaining the API key value from the response
-      NEW_API_KEY=$(echo "$API_KEY_RESPONSE" | jq -r .apikey)
-
-      # Saving the new API key value to the .env file
-      echo "APIKEY=$NEW_API_KEY" > .env
-      echo "NEW_APIKEY=$NEW_API_KEY"
-      echo "Congratulations! You have successfully obtained an API KEY and it's saved in .env."
       ;;
     "2")
       # Making an HTTP GET request to check the balance
@@ -117,20 +130,20 @@ while true; do
       else
         echo "No open orders found."
       fi
-      ;;  
+      ;;
     "7")
       # Asking the user whether to cancel a specific order or all orders
-      read -p "Do you want to cancel a specific order or all orders? (specific/all): " CANCEL_OPTION
+      read -p "Do you want to cancel a specific order or all orders? (1 for specific, 2 for all): " CANCEL_OPTION
 
       case "$CANCEL_OPTION" in
-        "specific")
+        "1")
           # Get the ID of the first open order
-          SPECIFIC_ORDER_ID=$(http GET https://cax.piccadilly.autonity.org/api/orders API-Key:$API | jq -r 'first(.[] | select(.status == "open") | .order_id)')
+          SPECIFIC_ORDER_ID=$(http GET https://cax.piccadilly.autonity.org/api/orders API-Key:$APIKEY | jq -r 'first(.[] | select(.status == "open") | .order_id)')
 
           # Check if there is an open order
           if [ -n "$SPECIFIC_ORDER_ID" ]; then
             # Making an HTTP DELETE request to cancel the specific order
-            http DELETE "https://cax.piccadilly.autonity.org/api/orders/$SPECIFIC_ORDER_ID" API-Key:$API
+            http DELETE "https://cax.piccadilly.autonity.org/api/orders/$SPECIFIC_ORDER_ID" API-Key:$APIKEY
 
             if [ $? -eq 0 ]; then
               echo "Order $SPECIFIC_ORDER_ID has been canceled successfully."
@@ -141,14 +154,14 @@ while true; do
             echo "No open orders to cancel."
           fi
           ;;
-        "all")
+        "2")
           # Getting all open order IDs
-          order_ids=$(http GET https://cax.piccadilly.autonity.org/api/orders API-Key:$API | jq -r '.[] | select(.status == "open") | .order_id')
+          order_ids=$(http GET https://cax.piccadilly.autonity.org/api/orders API-Key:$APIKEY | jq -r '.[] | select(.status == "open") | .order_id')
 
           if [ -n "$order_ids" ]; then
             # Canceling each open order
             for order_id in $order_ids; do
-              http DELETE "https://cax.piccadilly.autonity.org/api/orders/$order_id" API-Key:$API
+              http DELETE "https://cax.piccadilly.autonity.org/api/orders/$order_id" API-Key:$APIKEY
               echo "Order $order_id has been canceled successfully."
             done
 
@@ -163,12 +176,24 @@ while true; do
       esac
       ;;
     "8")
-      # Asking the user to choose a symbol (ATN or NTN)
-      read -p "Choose symbol (ATN or NTN): " SYMBOL
-      if [ "$SYMBOL" != "ATN" ] && [ "$SYMBOL" != "NTN" ]; then
-        echo "Invalid symbol."
-        continue
-      fi
+      # Displaying symbol options
+      echo "Symbol options:"
+      echo "1. ATN"
+      echo "2. NTN"
+      read -p "Choose symbol (1 for ATN, 2 for NTN): " SYMBOL_CHOICE
+
+      case "$SYMBOL_CHOICE" in
+        "1")
+          SYMBOL="ATN"
+          ;;
+        "2")
+          SYMBOL="NTN"
+          ;;
+        *)
+          echo "Invalid choice. Exiting the script."
+          exit 1
+          ;;
+      esac
 
       # Asking the user to enter the withdrawal amount
       read -p "Enter withdrawal amount: " AMOUNT
@@ -178,7 +203,7 @@ while true; do
       ;;
     "9")
       # Making an HTTP GET request to view deposit history
-      DEPOSIT_HISTORY=$(http GET "https://cax.piccadilly.autonity.org/api/deposits" "API-Key:$API")
+      DEPOSIT_HISTORY=$(http GET "https://cax.piccadilly.autonity.org/api/deposits" "API-Key:$APIKEY")
 
       if [ -n "$DEPOSIT_HISTORY" ]; then
         echo "Deposit History:"
@@ -188,7 +213,7 @@ while true; do
       fi
 
       # Making an HTTP GET request to view withdraw history
-      WITHDRAW_HISTORY=$(http GET "https://cax.piccadilly.autonity.org/api/withdraws" "API-Key:$API")
+      WITHDRAW_HISTORY=$(http GET "https://cax.piccadilly.autonity.org/api/withdraws" "API-Key:$APIKEY")
 
       if [ -n "$WITHDRAW_HISTORY" ]; then
         echo "Withdraw History:"
@@ -198,27 +223,26 @@ while true; do
       fi
       ;;
     "10")
-      echo "Exiting the script."
-      exit 0
+      # Asking the user if they want to go back to the menu or exit
+      read -p "Do you want to go back to the menu or exit? (1 for back, 2 for exit): " CONTINUE
+
+      case "$CONTINUE" in
+        "1")
+          # Continue the loop to go back to the menu
+          continue
+          ;;
+        "2")
+          echo "Exiting the script."
+          exit 0
+          ;;
+        *)
+          echo "Invalid choice. Exiting the script."
+          exit 1
+          ;;
+      esac
       ;;
     *)
       echo "Invalid choice."
-      continue
-      ;;
-  esac
-
-  # Asking the user if they want to go back to the menu or exit
-  read -p "Do you want to go back to the menu or exit? (back/exit): " CONTINUE
-  case "$CONTINUE" in
-    "back")
-      continue
-      ;;
-    "exit")
-      echo "Exiting the script."
-      exit 0
-      ;;
-    *)
-      echo "Invalid choice. Exiting the script."
       exit 1
       ;;
   esac
